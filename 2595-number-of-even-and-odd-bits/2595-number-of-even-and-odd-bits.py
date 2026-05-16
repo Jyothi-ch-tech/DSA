6aa7c26f9057bc8f7f2b7c19fe21e8201b71d7a1
chinthakula jyothi <chinthakulajyothi6@gmail.com>
2026-05-16 21:30:57 +0530

    Time: 0 ms (100%), Space: 18 MB (100%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
@@ -0,0 +1,18 @@
+class Solution:
+    def evenOddBit(self, n: int) -> List[int]:
+        counte=0
+        counto=0
+        i=1 
+        while n>0:
+            if n&1:
+                if i%2==0:
+                    counto+=1 
+                else:
+                    counte+=1 
+            n=n>>1 
+            i+=1 
+        return [counte,counto]
+        
+
+        
+        
